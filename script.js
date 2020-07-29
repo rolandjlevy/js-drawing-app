136400125b7c589e3948ff2a199612259bcb8b1c
Roland Levy <rolandjlevy@gmail.com>
2020-07-29 22:50:19 +0000

    Updated Blur object

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ colours.forEach(col => {
 const artBoard = new ArtBoard();
 const brush = new Brush({size:20});
 const opacity = new Opacity({value:50});
-const blurer = new Blur({value:10});
+const blurer = new Blur({value:5});
 
 const paint = new Paint({
   canvas: artBoard.canvas, 
